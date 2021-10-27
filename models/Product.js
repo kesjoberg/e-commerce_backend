576ecf6c858e8d41e8cbd4dd355a80a64a879502
Karna Sjoberg <kesjoberg@gmail.com>
2021-10-26 21:06:58 -0500

    models created

--- models/Product.js
+++ models/Product.js
@@ -9,8 +9,41 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true,  
+      },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        len: [10],
+      },
+    },
+    category_id: {
+      type: DataType.INTEGER,
+      references:{
+        model: 'category',
+        key: 'id',
+      },
+    }
     // define columns
   },
+
   {
     sequelize,
     timestamps: false,
--- models/index.js
+++ models/index.js
@@ -3,14 +3,21 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const { belongsToMany } = require('../../../inclass/unit13-ORM/01-Activities/28-Stu_Mini-Project/unit13-mini-project/models/Traveller');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: '',
+});
 // Categories have many Products
-
+Category.hasMany(Product,{
+  foreignKey:'',
+  onDelete: 'CASCADE',
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, { through: ProductTag });
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, { through: ProductTag } );
 
 module.exports = {
   Product,
